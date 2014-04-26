47b7a4285ba23875a1bff516712ae025cd08bd40
Doug <dougjchau@gmail.com>
2014-04-26 13:34:30 -0400

    add pages to each link

--- schedulizer/urls.py
+++ schedulizer/urls.py
@@ -3,4 +3,8 @@ from schedulizer import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'addclasses', views.addClasses, name='addClasses'),
+    url(r'finalschedule', views.finalSchedule, name='finalSchedule'),
+    url(r'inputdars', views.inputDars, name='inputDars'),
+
 ]
--- schedulizer/views.py
+++ schedulizer/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render_to_response
 
 def index(request):
 	return render_to_response('schedulizer/index.html')
+
+def addClasses(request):
+	return render_to_response('schedulizer/addClasses.html')
+
+def finalSchedule(request):
+	return render_to_response('schedulizer/finalSchedule.html')
+
+def inputDars(request):
+	return render_to_response('schedulizer/inputDars.html')
