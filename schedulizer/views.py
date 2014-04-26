75f398e13bc953c091131685ee6b45574d3d53ac
Leon Lee <93leonlee@gmail.com>
2014-04-26 18:08:37 -0400

    courses now passed onto the view

--- schedulizer/views.py
+++ schedulizer/views.py
@@ -3,13 +3,18 @@ from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_protect
 from django.template import RequestContext
 from forms import CourseForm
+from schedulizer.models import *
 
 def index(request):
 	return render_to_response('schedulizer/index.html')
 
 def addClasses(request):
 	if request.method == 'POST':
-		print request.POST
+		courses = request.POST.getlist('course')
+		courseList = []
+		for course in courses:
+			courseList.append(Course.objects.get(id = course))
+		print 'courses inputted', courseList
 	form = CourseForm()
 	return render_to_response('schedulizer/addClasses.html',{
 		'form':form,
